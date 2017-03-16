57393b5b81364c884f28b41e38691896acb8614d
Mr. Le Bosniaque <alvin.m@codeur.online>
2017-03-16 13:46:49 +0100

    javascript de la page de recherche commenté

--- public/js/navCriteresRecherche.js
+++ public/js/navCriteresRecherche.js
@@ -5,36 +5,40 @@ $(document).ready(function() {
 	var parAuteur = $('#articleCriteresAuteur');
 	var parTitre = $('#articleCriteresTitre');
 
-	$('#boutonVilleSuivant').on('click', function(event) {
+  //------------------------------------------------------------------------------------------------------------
+
+	$('#boutonVilleSuivant').on('click', function(event) {   //navigation des articles de recherche superposés sur les fleches
 		event.preventDefault();
 		parVille.animate({left: '-200%'});
 
 	});
 
-	$('#boutonAuteurSuivant').on('click', function(event) {
+	$('#boutonAuteurSuivant').on('click', function(event) {    //navigation des articles de recherche superposés sur les fleches
 		event.preventDefault();
 		parAuteur.animate({left: '-200%'});
 
 	});
 
-	$('#boutonTitrePrecedent').on('click', function(event) {
+	$('#boutonTitrePrecedent').on('click', function(event) {   //navigation des articles de recherche superposés sur les fleches
 		event.preventDefault();
 		parAuteur.animate({left: '9%'});
 
 	});
 
-	$('#boutonAuteurPrecedent').on('click', function(event) {
+	$('#boutonAuteurPrecedent').on('click', function(event) {    //navigation des articles de recherche superposés sur les fleches
 		event.preventDefault();
 		parVille.animate({left: '5%'});
 
 	});
 
-	$('#submit').on('click', function() {
+  //------------------------------------------------------------------------------------------------------------
+
+	$('#submit').on('click', function() {  //soumission du formulaire au click sur la loupe
 		$('#formRecherche').submit();
 	});
-
+//------------------------------------------------------------------------------------------------------------
         
-        $('#parVille').keyup(function(e){
+        $('#parVille').keyup(function(e){//ajax, envoi de donnée de la recherche par ville
         	$('#recupAjaxVille').html('');
         	var code = e.which;
         	if(code == 16 || code == 20) {
@@ -42,7 +46,7 @@ $(document).ready(function() {
     		}
 
     		if($('#parVille').val().length !== 0) {
-				$('#recupAjaxVille').css('visibility', 'visible');
+				$('#recupAjaxVille').css('visibility', 'visible');  //ajax, envoi de donnée de la recherche par ville
 
     		} else if($('#parVille').val().length == 0) {
 
@@ -53,7 +57,7 @@ $(document).ready(function() {
                 type:'POST',
                  url:'rechercheVille',
                  data: {
-                    "_token": $('#token').attr('value'),
+                    "_token": $('#token').attr('value'),//ajax, envoi de donnée de la recherche par ville
                     "villes": $('#parVille').val()
                     },
                  success:function(data){
@@ -62,7 +66,7 @@ $(document).ready(function() {
 
               		}
                     
-                    $('.listeVilles').on('click', function(e) {
+                    $('.listeVilles').on('click', function(e) {//ajax, envoi de donnée de la recherche par ville
                     	e.preventDefault();
         				$('#parVille').val($(this).html());
         				$('#recupAjaxVille').css('visibility', 'hidden');
@@ -71,38 +75,38 @@ $(document).ready(function() {
                 }
             });
         });
-
+//------------------------------------------------------------------------------------------------------------
         $('#parAuteur').keyup(function(e){
 
         	$('#recupAjaxAuteur').html('');
         	var code = e.which;
-        	if(code == 16 || code == 20) {
+        	if(code == 16 || code == 20) {//ajax, envoi de donnée de la recherche par auteur
        			return;
     		}
 
     		if($('#parAuteur').val().length !== 0) {
-				$('#recupAjaxAuteur').css('visibility', 'visible');
+				$('#recupAjaxAuteur').css('visibility', 'visible');//ajax, envoi de donnée de la recherche par auteur
 
     		} else if($('#parAuteur').val().length == 0) {
 
-				$('#recupAjaxAuteur').css('visibility', 'hidden');
+				$('#recupAjaxAuteur').css('visibility', 'hidden');//ajax, envoi de donnée de la recherche par auteur
     		}
 
             $.ajax({
                 type:'POST',
                  url:'rechercheAuteur',
                  data: {
-                    "_token": $('#token').attr('value'),
+                    "_token": $('#token').attr('value'),//ajax, envoi de donnée de la recherche par auteur
                     "auteurs": $('#parAuteur').val()
                     },
                  success:function(data){
 
-                      for (var k = 0; k < data.villesAuteur.length ; k++) {
+                      for (var k = 0; k < data.villesAuteur.length ; k++) {//ajax, envoi de donnée de la recherche par auteur
                 		$('#recupAjaxAuteur').append('<a href="" class="listeAuteurs" style="margin: 0px; padding: 0px; text-decoration: none;">' + data.villesAuteur[k].auteur_principal_nom_700a +'</a><br>');
 
               		}  
 
-              		$('.listeAuteurs').on('click', function(e) {
+              		$('.listeAuteurs').on('click', function(e) {//ajax, envoi de donnée de la recherche par auteur
               			e.preventDefault();
         				$('#parAuteur').val($(this).html());
         				$('#recupAjaxAuteur').css('visibility', 'hidden');
@@ -110,17 +114,17 @@ $(document).ready(function() {
                 }
             });
         });
-
+//------------------------------------------------------------------------------------------------------------
         $('#parTitre').keyup(function(e){
 
         	$('#recupAjaxTitre').html('');
         	var code = e.which;
-        	if(code == 16 || code == 20) {
+        	if(code == 16 || code == 20) {//ajax, envoi de donnée de la recherche par titre de livre
        			return;
     		}
 
     		if($('#parTitre').val().length !== 0) {
-				$('#recupAjaxTitre').css('visibility', 'visible');
+				$('#recupAjaxTitre').css('visibility', 'visible');//ajax, envoi de donnée de la recherche par titre de livre
 
     		} else if($('#parTitre').val().length == 0) {
 
@@ -131,17 +135,17 @@ $(document).ready(function() {
                 type:'POST',
                  url:'rechercheTitre',
                  data: {
-                    "_token": $('#token').attr('value'),
+                    "_token": $('#token').attr('value'),//ajax, envoi de donnée de la recherche par titre de livre
                     "titres": $('#parTitre').val()
                     },
                  success:function(data){
 
-                      for (var k = 0; k < data.villesTitre.length ; k++) {
+                      for (var k = 0; k < data.villesTitre.length ; k++) {//ajax, envoi de donnée de la recherche par titre de livre
                 		$('#recupAjaxTitre').append('<a href="" class="listeTitres" style="margin: 0px; padding: 0px; text-decoration: none;">' + data.villesTitre[k].titre_200a +'</a><br>');
 
               		}  
 
-              		$('.listeTitres').on('click', function(e) {
+              		$('.listeTitres').on('click', function(e) {//ajax, envoi de donnée de la recherche par titre de livre
               			e.preventDefault();
         				$('#parTitre').val($(this).html());
         				$('#recupAjaxTitre').css('visibility', 'hidden');
@@ -151,12 +155,12 @@ $(document).ready(function() {
         });
 
 
-
+//------------------------------------------------------------------------------------------------------------
     if ($(window).width() < 1000) {
 
         $('#imageMenuLo').attr('src', 'css/styleAlvin/localisation-couleur.png');
 
-        $('#menuVille').on('click', function(e) {
+        $('#menuVille').on('click', function(e) { //Navigation entre les articles supérposé au clique sur les icones du menu
           e.preventDefault();
           $('#imageMenuLo').attr('src', 'css/styleAlvin/localisation-couleur.png');
           $('#imageMenuAu').attr('src', 'css/styleAlvin/auteur.png');
@@ -164,7 +168,7 @@ $(document).ready(function() {
            parAuteur.animate({left: '9%'});
            parVille.animate({left: '5%'});
         });
-
+//Navigation entre les articles supérposé au clique sur les icones du menu
 
         $('#menuAuteur').on('click', function(e) {
           $('#imageMenuLo').attr('src', 'css/styleAlvin/localisation.png');
@@ -174,7 +178,7 @@ $(document).ready(function() {
             parVille.animate({left: '-200%'});
             parAuteur.animate({left: '9%'});
         });
-
+//Navigation entre les articles supérposé au clique sur les icones du menu
 
         $('#menuTitre').on('click', function(e) {
           $('#imageMenuLo').attr('src', 'css/styleAlvin/localisation.png');
